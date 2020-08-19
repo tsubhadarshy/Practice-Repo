c6a38166b3937fe3d5a0e6a185fbe4f76810f3fd
SirKitBreaker <sssahil4713@gmail.com>
2020-08-19 10:43:24 +0530

    Updated Activity1_1.java

--- Activity1_1.java
+++ Activity1_1.java
@@ -5,6 +5,7 @@ public class Activity1_1 {
     public static void main(String[] args) {
 	    // Object initialization
 	    Car toyota = new Car();
+		// Add default values
 	    toyota.make = 2014;
 	    toyota.color = "Black";
 	    toyota.transmission = "Manual";
