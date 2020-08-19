03e4d532fcba0c15252b49cc1e69dbb8e25ec4c4
SirKitBreaker <50264869+SirKitBreaker@users.noreply.github.com>
2020-08-19 10:40:38 +0530

    Update Activity1_1.java

--- Activity1_1.java
+++ Activity1_1.java
@@ -3,9 +3,10 @@ package session1;
 public class Activity1_1 {
 
     public static void main(String[] args) {
-    	Car toyota = new Car();
-		toyota.make = 2014;
-		toyota.color = "Black";
+	    // Object initialization
+	    Car toyota = new Car();
+	    toyota.make = 2014;
+	    toyota.color = "Black";
 	    toyota.transmission = "Manual";
 	
 		//Using Car class method
