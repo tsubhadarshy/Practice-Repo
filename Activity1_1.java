4c6fe17822eebb0d61d43841bc3364ab3c6d6094
RakeshGoli <70194091+RakeshGoli@users.noreply.github.com>
2020-10-07 11:55:38 +0530

    Update Activity1_1.java

--- Activity1_1.java
+++ Activity1_1.java
@@ -9,7 +9,7 @@ public class Activity1_1 {
 	    toyota.make = 2015;
 	    toyota.color = "White";
 	    toyota.transmission = "Manual";
-	    toyota.modelNumber = "qpri134";
+	    toyota.modelNumber = "qpri1344";
 	
 		//Using Car class method
 		toyota.displayCharacterstics();
